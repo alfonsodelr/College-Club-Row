098f5b39c049986c4b71ab49e73bc7c27a65bd12
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-01-31 00:02:52 -0800

    #

--- next-auth/TODO.md
+++ next-auth/TODO.md
@@ -1,14 +1,35 @@
  1/25/2022
  //TODO:
-        //1. get user permission profile in api/auth/[...nextauth].js line 77.
-        //2. add special permisson if necessary
-        //3. add middleware to club pages
-        //4. check permission before each club action.
+        //5. improve api/user checkError method
 
 
-//1/26/2022 TODO: 
-1. go to api/user/index.js and finish createDefaultUserIfUndefined. preferebly with one pipeline. and I don't think we need to change the getItem dynamodb function return...... maybe not; get back to that again. 
+
+-------------------------Wrok Hours Log----------------------------
+start: 10:43 pm 1/28  
+
+1/30 start: 11:28   end: 1/31/ 00:02
+
++ middleware to club route
++ filewriter helper for logging
++ api param type for typescript funcitons
++ getuser with unmarshell data
++ @aws-sdk/util-dynamodb
++ marshell and unmarshell with util-dynamodb
++ debug api routes
 
 
-//changed get user params
-//+ api/user GET: create user if user is undefined.
+
+
+
+
+------------------------fix later----------------------------
+
+api/user/index.ts; getUser():  this can be cleaned up later since I returned the param itself from getItem() 
+ api endpoints can be improved by using dynamoDB marshell() and unmarshell()
+
+
+
+----------------------------Documentation------------------------
+TotalID count: 12
+
+@DocumentID=12 : means specific docuemtns are written at Document=12 tag. 
--- next-auth/src/utils/helper.ts
+++ next-auth/src/utils/helper.ts
@@ -1,3 +1,24 @@
+import axios from "axios"
+import { ApiUserGETQueryParamType as GetUserParamType } from '../pages/api/apiParamType'
+// import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+const baseUrl = process.env.NEXT_PUBLIC_ORIGIN_RUL;
+
+
+function generateDefaultUser(param: generateDefaultUserType) {
+    var data: userType = {
+        userID: param.userID,
+        clubs: [],
+        tasks: [],
+        userName: param.legalName,
+        legalName: param.legalName,
+        role: [
+            "member"
+        ],
+        email: param.email,
+    }
+    return data;
+}
 
 function varNameGenerator(str, timeStemp = true, time = Date.now()) {
     if (typeof str !== 'string') throw new Error("utils/helper.ts: param error.");
@@ -12,4 +33,28 @@ const Tap = (fn = console.log) => x => (fn(x), x);
 const Pipe = (...fns) => fns.reduce((result, f) => (...args) => f(result(...args)));
 
 
-export { varNameGenerator, Tap, Pipe }
+
+async function getUserProfile(userData: GetUserParamType) {
+    const res = await axios.get(baseUrl + "/api/user/", { params: { ...userData } })
+    let data: userType = res.data.Item
+    return { data, status: res.status }
+}
+
+type userType = {
+    userID: number | string,
+    clubs: Array<string>,
+    tasks: Array<string>,
+    userName: string,
+    legalName: string,
+    role: Array<string>,
+    email: string,
+}
+
+type generateDefaultUserType = {
+    userID: string,
+    email: string,
+    legalName: string,
+}
+
+export { varNameGenerator, Tap, Pipe, getUserProfile, generateDefaultUser }
+export type { userType }
