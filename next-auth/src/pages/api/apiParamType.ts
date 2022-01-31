9967a6f18c2e2dce9dd9b9c9703f45879c9b4f26
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-01-31 00:01:18 -0800

    + APIUSerGETQueryParamType. + unauth page to redirect unauthenticated users.  moved generateDefaultUser from api/user to Utils. moved userType, generateDefaultUserType to utils. bugfix on getUserProfile().

---
+++ next-auth/src/pages/api/apiParamType.ts
@@ -0,0 +1,9 @@
+
+type ApiUserGETQueryParamType = {
+    userID: number | string,
+    email: string,
+    legalName: string,
+}
+
+
+export type { ApiUserGETQueryParamType }
---
+++ next-auth/src/pages/unauth.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function unauth() {
+    return <div>You are not authorized to access this page</div>;
+}
+
+export default unauth;
