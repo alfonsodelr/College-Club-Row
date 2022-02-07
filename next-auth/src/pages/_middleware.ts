6872a319406e87a1d7243ebf0e02de84eca9f8c9
yasen002 <46136113+yasen002@users.noreply.github.com>
2022-02-06 18:45:27 -0800

    + club middleware
    now club middleware detects club roles and redirect to associated pages.

--- next-auth/src/pages/_middleware.ts
+++ next-auth/src/pages/_middleware.ts
@@ -4,19 +4,9 @@ import { NextRequest, NextResponse } from "next/server"
 /** @param {import("next/server").NextRequest} req */
 export async function middleware(req) {
 
-  console.log("index middleWare index is running")
+  try {
+    console.log("index middleWare index is running")
 
-  const session = await getToken({
-    req,
-    secret: process.env.SECRET,
-    secureCookie:
-      process.env.NEXTAUTH_URL?.startsWith("https://") ??
-      !!process.env.VERCEL_URL,
-  })
-
-  console.log(session)
-
-  if (req.nextUrl.pathname === "/middleware-protected") {
     const session = await getToken({
       req,
       secret: process.env.SECRET,
@@ -25,12 +15,26 @@ export async function middleware(req) {
         !!process.env.VERCEL_URL,
     })
 
-    console.log(session)
-    // console.log("this session is", session, "session.....")
-    // You could also check for any property on the session object,
-    // like role === "admin" or name === "John Doe", etc.
-    if (!session) return NextResponse.redirect("/api/auth/signin")
-    // If user is authenticated, continue.
+    // console.log(session)
+
+    if (req.nextUrl.pathname === "/middleware-protected") {
+      const session = await getToken({
+        req,
+        secret: process.env.SECRET,
+        secureCookie:
+          process.env.NEXTAUTH_URL?.startsWith("https://") ??
+          !!process.env.VERCEL_URL,
+      })
+
+      // console.log(session)
+      // console.log("this session is", session, "session.....")
+      // You could also check for any property on the session object,
+      // like role === "admin" or name === "John Doe", etc.
+      if (!session) return NextResponse.redirect("/api/auth/signin")
+      // If user is authenticated, continue.
+    }
+  } catch (error) {
+    console.log("/_middleware: " + error.message)
   }
 }
 
--- next-auth/src/pages/club/_middleware.ts
+++ next-auth/src/pages/club/_middleware.ts
@@ -1,21 +1,69 @@
 
-// This is an example of how to read a JSON Web Token from an API route
+// // This is an example of how to read a JSON Web Token from an API route
+// import axios from "axios"
+// import { NextResponse } from "next/server"
+// /*----TODO----
+// 1.  
+
+// */
+
+// export default async function middleware(req, res) {
+//     // console.log("club Middleware is running", req.cookies)
+//     // console.log("club Middleware is running", baseUrl)
+//     // const clubResponse = await axios.get(baseUrl + "/api/club/", { params: { clubID: "111" } })
+//     // console.log(clubResponse);
+
+//     ///------temparary disabling this for testing
+//     // const token: any = await getToken({ req, secret })
+//     // const role = token.profiles.role;
+//     // if (!token || !role.includes("president") || !role.includes("officer")) return NextResponse.redirect("/unauth")
+
+//     // // console.log("JSON Web Token", JSON.stringify(token, null, 2))
+//     // console.log("club middleware is running")
+
+
+//     return NextResponse.next()
+
+// }
+
+//Purpose of middleware: how you manipulate and configure a response, based on the incoming requests.
+
 import { getToken } from "next-auth/jwt"
-import { NextResponse } from "next/server"
-import { userType } from "../../utils/helper"
-const secret = process.env.SECRET
+import { NextRequest, NextResponse } from "next/server"
+import { getClubName, getClubRoleFromSession } from "../../utils/helper"
+import { sessionType } from "../../utils/schema"
+/** @param {import("next/server").NextRequest} req */
+export async function middleware(req) {
+    try {
+        if (req.nextUrl.pathname !== "/club") return NextResponse.next();
+        console.log("club middleWare is running")
 
-export default async function middleware(req, res) {
+        var session: any = await getToken({
+            req,
+            secret: process.env.SECRET,
+            secureCookie:
+                process.env.NEXTAUTH_URL?.startsWith("https://") ??
+                !!process.env.VERCEL_URL,
+        })
 
-    ///------temparary disabling this for testing
-    // const token: any = await getToken({ req, secret })
-    // const role = token.profiles.role;
-    // if (!token || !role.includes("president") || !role.includes("officer")) return NextResponse.redirect("/unauth")
+        if (!session) return NextResponse.redirect("/api/auth/signin")
 
-    // // console.log("JSON Web Token", JSON.stringify(token, null, 2))
-    // console.log("club middleware is running")
+        var roleArr = session.profiles.role;
+        const { role, numOfRole } = getClubRoleFromSession(roleArr)
 
+        if (numOfRole === 0) return NextResponse.redirect("/");
 
-    return NextResponse.next()
+        if (numOfRole > 1) {
+            //have to fix this seanrio later!!!!!
+            //warning more than one role
+        }
 
+        var userRole = role[0][0];
+        var userClub = role[0][1];
+        var clubName = getClubName(parseInt(userClub));
+        return NextResponse.redirect(`/club/${clubName}`)
+
+    } catch (error) {
+        console.log("club/_middleware: ", error.message)
+    }
 }
